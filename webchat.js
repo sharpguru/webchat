c3a88597c5ad14518339a5c71bc4b3d149a1cda5
Brian Foote <brian@gurubyte.com>
2017-07-17 23:55:43 -0700

    Added some local file commands

--- webchat.js
+++ webchat.js
@@ -1,5 +1,8 @@
 var socket = require('socket.io-client')('http://localhost:1337');
+var fs = require('fs');
+var path = require('path');
 var connectionid = '';
+var currentDir = __dirname;
 
 socket.on('connect', function(){
 });
@@ -39,51 +42,26 @@ function prompt() {
       var space = msg.indexOf(' ');
       if (space == -1) space = msg.length;
       var cmd = msg.substring(1, space);
+      var cmdlower = cmd.toLowerCase();
       var data = msg.substring(space + 1);
       //console.log(space);
       //console.log(data);
       //console.log(cmd);
 
       // check for local commands
-      if ("share" == cmd.toLowerCase()) {
-        var arr = data.split(' ');
-        var localpath = arr[0];
-        var sharename = arr[1];
-
-        if (!localpath || !sharename) {
-          // list shares
-          socket.emit('share', '');
-        } else {
-          console.log('setting share');
-
-          // Set share
-
-          // validate localpath
-          //var myFileURL = new URL('file://C:/data'); // maybe someday
-          // TODO: Add support for windows drive letters and absolute urls
-          var pathname = path.join(__dirname, localpath);
-          console.log(pathname);
-          fs.access(pathname, (err) => {
-            if (err) {
-              if (err.code === 'ENOENT') {
-                console.error('path does not exist');
-              }
-            } else {
-              var share = {
-                name: sharename,
-                path: localpath,
-                userid: id
-              }; // TODO: Replace user id with more permanent user public key
-
-              //db.get('shares').push(share).write();
-              //socket.emit('returnmessage', 'share added!');
-              socket.emit('share', share);
-            }
-          });
-        }
-
-      } else { // send remote command
-        socket.emit(cmd, data);
+      switch (cmdlower) {
+        case "cd":
+          commandCD(data);
+          break;
+        case "dir":
+          commandDir();
+          break;
+        case "share":
+          commandShare(data);
+          break;
+        default :
+          socket.emit(cmd, data);
+          break;
       }
     } else {
       socket.emit('chat', msg);
@@ -91,6 +69,74 @@ function prompt() {
     //rl.close();
     prompt();
   });
+}
+
+function commandCD(data) {
+  if (data.length == 0) {
+    console.log(currentDir);
+  } else {
+    // change dir
+    var isDir = fs.statSync(currentDir).isDirectory();
+    if (isDir) {
+      if (data == '..') {
+        // go up
+        currentDir = path.resolve(currentDir, '..');
+        console.log(currentDir);
+      } else {
+        currentDir = path.resolve(currentDir, data);
+        console.log(currentDir);
+      }
+    } else {
+      console.log(currentDir);
+    }
+  }
+
+}
+
+function commandDir() {
+  var dircontents = fs.readdirSync(currentDir);
+  dircontents.forEach(function (filename) {
+    console.log(filename);
+  })
+}
+
+function commandShare(data) {
+  var arr = data.split(' ');
+  var localpath = arr[0];
+  var sharename = arr[1];
+
+  if (!localpath || !sharename) {
+    // list shares
+    console.log('shares:');
+    socket.emit('share', data);
+  } else {
+    console.log('setting share:');
+
+    // Set share
+
+    // validate localpath
+    //var myFileURL = new URL('file://C:/data'); // maybe someday
+    // TODO: Add support for windows drive letters and absolute urls
+    var pathname = path.join(__dirname, localpath);
+    console.log(pathname);
+    fs.access(pathname, (err) => {
+      if (err) {
+        if (err.code === 'ENOENT') {
+          console.error('path does not exist');
+        }
+      } else {
+        var share = {
+          name: sharename,
+          path: localpath,
+          userid: id
+        }; // TODO: Replace user id with more permanent user public key
+
+        //db.get('shares').push(share).write();
+        //socket.emit('returnmessage', 'share added!');
+        socket.emit('share', share);
+      }
+    });
+  }
 }
 
 //prompt();
